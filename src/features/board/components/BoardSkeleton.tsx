5708e5e601e29247c4047d7d25cfe2c18aaaac89
Julee-Kim <jlkim0323@kakao.com>
2026-01-11 22:53:38 +0900

    보드 페이지 스켈레톤 스타일 변경

--- src/features/board/components/BoardSkeleton.tsx
+++ src/features/board/components/BoardSkeleton.tsx
@@ -3,14 +3,19 @@ import styles from './BoardSkeleton.module.css'
 const BoardSkeleton = () => {
   return (
     <div className={styles.board}>
-      {[0, 1, 2].map((i) => (
-        <div key={i} className={styles.column}>
-          <div className={styles.title} />
-          <div className={styles.card} />
-          <div className={styles.card} />
-          <div className={styles.card} />
-        </div>
-      ))}
+      <div className={styles.column}>
+        <div className={styles.title} />
+        <div className={styles.card} />
+        <div className={styles.card} />
+      </div>
+      <div className={styles.column}>
+        <div className={styles.title} />
+        <div className={styles.card} />
+      </div>
+      <div className={styles.column}>
+        <div className={styles.title} />
+        <div className={styles.card} />
+      </div>
     </div>
   )
 }
