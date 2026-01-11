57cb66cdf328359e992155412ff4cab9dd78415d
Julee-Kim <jlkim0323@kakao.com>
2026-01-11 17:18:27 +0900

    toast 라이브러리 설치 및 세팅

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,19 @@
 import { RouterProvider } from 'react-router-dom'
 import router from '@/router'
 import QueryProvider from '@/providers/query/QueryProvider'
+import { Toaster } from 'sonner'
 import './styles/index.css'
 
 const App = () => {
   return (
     <QueryProvider>
       <RouterProvider router={router} />
+      <Toaster
+        position="top-right"
+        richColors
+        expand // 여러개가 겹치지 않는 옵션
+        toastOptions={{ style: { width: '300px' } }}
+      />
     </QueryProvider>
   )
 }
